846202706ff9ef5841ac55ba71b11be555225ee4
Andrei Garcia <andrei@datamex.com.br>
2023-09-01 02:35:53 -0300

    criação de testes untários da classe campanha

--- Tests/CampanhaTest.php
+++ Tests/CampanhaTest.php
@@ -12,7 +12,14 @@ class CampanhaTest  extends TestCase {
 
     public function testObtemCampanha(){
         $campanha = new CampanhaModels();
-        $ret = $campanha->select(1);
+        $ret = $campanha->select(5);
         $this->assertNotEmpty($ret);
+    }
+
+
+    public function testDeleteCampanha(){
+        $campanha = new CampanhaModels();
+        $ret = $campanha->delete(6);
+        $this->assertEquals("campanha removida com sucesso",$ret);
     }
 }
